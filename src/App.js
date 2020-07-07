fa067543fc7cb73c264e6d725fe7f0d5a9806dbe
STELLAMARUH <stellamaruh@gmail.com>
2020-07-07 08:37:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     <h1>Maama Mboga</h1>
+     <h2>Welcome to Green Kiosk</h2>
+     <h3> mama mboga</h3>
+     class Mummboga()
+      constructor()
+      this.product=[] 
+      this.carts=[]
+      this.orders=[] 
+      this.customer=[]
+      
+     <div>
+      
+       addProduct(productId,name,price,quantity)
+       let product
+         productId
+         name:name
+         price:price
+         quantity:quantity
+       
+     </div>
+     
+
+      
+    </div>
+    
+
+  );
+}
+
+export default App;
